4cd022deb507d7797032a669d46042420a6fc194
u1810291 <o.rizakulov@student.inha.uz>
2021-05-01 15:34:46 +0500

    table almost done

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,17 +1,67 @@
 
-export default ()=> {
+
+export default (props: any) => {
   return (
     <div>
-      <table>
-        <th>
-          <tr>Head</tr>
-        </th>
-        <tr>
-          <td>
-            some
-          </td>
-        </tr>
-      </table>
-    </div>
+      {props.headers ?
+        <table>
+          <thead>
+            {console.log(props.headers)}
+            <tr >
+              {props.headers.map(({ header }: any, index: number) =>
+                <th key={index + 1}>{header}</th>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {props.data.map(({
+              id,
+              name,
+              morning,
+              first_check,
+              secon_check,
+              third_check,
+              fourth_check,
+              total_attends,
+              total_absence,
+              absence_hours
+            }: any, index: number) =>
+              <tr key={index + 1}>
+                <td>
+                  {id}
+                </td>
+                <td>
+                  {name}
+                </td>
+                <td>
+                  {morning}
+                </td>
+                <td>
+                  {first_check}
+                </td>
+                <td>
+                  {secon_check}
+                </td>
+                <td>
+                  {third_check}
+                </td>
+                <td>
+                  {fourth_check}
+                </td>
+                <td>
+                  {total_attends}
+                </td>
+                <td>
+                  {total_absence}
+                </td>
+                <td>
+                  {absence_hours}
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table> : null
+      }
+    </div >
   )
 }
---
+++ src/views/Attendees/List/helper.ts
@@ -0,0 +1,57 @@
+
+export const headers = [
+  {
+    header: 'ID',
+    accessor: 'id'
+  },
+  {
+    header: "Name",
+    accessor: 'name'
+  },
+  {
+    header: "Morning check",
+    accessor: 'morning'
+  },
+  {
+    header: "First check",
+    accessor: 'first_check'
+  },
+  {
+    header: "Second check",
+    accessor: 'secon_check'
+  },
+  {
+    header: "Third check",
+    accessor: 'third_check'
+  },
+  {
+    header: "Fourth check",
+    accessor: 'fourth_check'
+  },
+  {
+    header: "Total attends",
+    accessor: 'total_attends'
+  },
+  {
+    header: "Total absence",
+    accessor: 'total_absence'
+  },
+  {
+    header: "Absence hours",
+    accessor: 'absence_hours'
+  }
+]
+export const data = [
+  {
+    id: 'ID',
+    name: 'Name',
+    morning: 'morning',
+    first_check: 'first_check',
+    secon_check: 'secon_check',
+    third_check: 'third_check',
+    fourth_check: 'fourth_check',
+    total_attends: 'total_attends',
+    total_absence: 'total_absence',
+    absence_hours: 'absence_hours'
+  }
+]
