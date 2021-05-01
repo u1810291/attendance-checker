45f87bccaa15f5aaa181918a429c07f7e114a6f5
u1810291 <o.rizakulov@student.inha.uz>
2021-05-01 16:57:09 +0500

    lets check

---
+++ src/components/Table/helper.ts
@@ -0,0 +1,8 @@
+import React from 'react'
+import Images from './Image'
+
+
+export const headerMaker = (datas: any) => datas
+  .map(({ type, rest }: any) => {
+
+  })
--- src/views/Attendees/List/helper.ts
+++ src/views/Attendees/List/helper.ts
@@ -2,68 +2,193 @@
 export const headers = [
   {
     header: 'ID',
-    accessor: 'id'
+    accessor: 'id',
+    type: 'id'
   },
   {
-    header: "Name",
-    accessor: 'name'
+    header: "First name",
+    accessor: 'first_name',
+    type: 'name'
   },
   {
-    header: "Morning check",
-    accessor: 'morning'
+    header: "Last name",
+    accessor: 'last_name',
+    type: 'name'
   },
   {
-    header: "First check",
-    accessor: 'first_check'
+    header: "list id",
+    accessor: 'list_id',
+    type: 'id'
   },
   {
-    header: "Second check",
-    accessor: 'secon_check'
+    header: "Avatar id",
+    accessor: 'avatar_image_id',
+    type: 'id'
   },
   {
-    header: "Third check",
-    accessor: 'third_check'
+    header: "Urls",
+    accessor: 'urls',
+    type: 'image'
   },
   {
-    header: "Fourth check",
-    accessor: 'fourth_check'
+    header: "Images",
+    accessor: 'images',
+    type: 'image'
   },
   {
-    header: "Total attends",
-    accessor: 'total_attends'
+    header: "Plates",
+    accessor: 'plates',
+    type: 'image'
   },
   {
-    header: "Total absence",
-    accessor: 'total_absence'
-  },
-  {
-    header: "Absence hours",
-    accessor: 'absence_hours'
+    header: "Plates count",
+    accessor: 'plates_count',
+    type: 'id'
   }
 ]
 export const data = [
   {
-    id: 'ID',
-    name: 'Name',
-    morning: 'morning',
-    first_check: 'first_check',
-    secon_check: 'secon_check',
-    third_check: 'third_check',
-    fourth_check: 'fourth_check',
-    total_attends: 'total_attends',
-    total_absence: 'total_absence',
-    absence_hours: 'absence_hours'
+    id: 17282,
+    first_name: "google",
+    last_name: "google",
+    list_id: 1158,
+    avatar_image_id: 266292,
+    urls: [],
+    images: [
+      {
+        id: 266292,
+        alg_type: 2,
+        image: "/api/v1/faces/17282/images/266292/image?imghash=2cd66178e44a59847b6bd1b08ce78ca8",
+        quality: 0.6115514,
+        major_version: 20200706,
+        minor_version: 1,
+        image_hash: "2cd66178e44a59847b6bd1b08ce78ca8",
+        face_id: 17282,
+        image_thumbnail: "/api/v1/faces/17282/images/266292/image-thumbnail?imghash=2cd66178e44a59847b6bd1b08ce78ca8",
+        status: "OK"
+      }
+    ],
+    plates: [],
+    plates_count: 0
   },
   {
-    id: 'ID',
-    name: 'Name',
-    morning: 'morning',
-    first_check: 'first_check',
-    secon_check: 'secon_check',
-    third_check: 'third_check',
-    fourth_check: 'fourth_check',
-    total_attends: 'total_attends',
-    total_absence: 'total_absence',
-    absence_hours: 'absence_hours'
+    id: 17281,
+    first_name: "agent111",
+    last_name: "agent",
+    list_id: 1158,
+    avatar_image_id: 266291,
+    urls: [],
+    images: [
+      {
+        id: 266291,
+        alg_type: 2,
+        image: "/api/v1/faces/17281/images/266291/image?imghash=d53b481440d7314b286472cdeb4ceb99",
+        quality: 0.49462447,
+        major_version: 20200706,
+        minor_version: 1,
+        image_hash: "d53b481440d7314b286472cdeb4ceb99",
+        face_id: 17281,
+        image_thumbnail: "/api/v1/faces/17281/images/266291/image-thumbnail?imghash=d53b481440d7314b286472cdeb4ceb99",
+        status: "OK"
+      }
+    ],
+    plates: [],
+    plates_count: 0
+  },
+  {
+    id: 17280,
+    first_name: "Shuxrat",
+    last_name: "eqweqw",
+    list_id: 1158,
+    avatar_image_id: 266290,
+    urls: [],
+    images: [
+      {
+        id: 266290,
+        alg_type: 2,
+        image: "/api/v1/faces/17280/images/266290/image?imghash=a3cf2f895a39ac9daaf38ed7f4b5a695",
+        quality: 0.75253224,
+        major_version: 20200706,
+        minor_version: 1,
+        image_hash: "a3cf2f895a39ac9daaf38ed7f4b5a695",
+        face_id: 17280,
+        image_thumbnail: "/api/v1/faces/17280/images/266290/image-thumbnail?imghash=a3cf2f895a39ac9daaf38ed7f4b5a695",
+        status: "OK"
+      }
+    ],
+    plates: [],
+    plates_count: 0
+  },
+  {
+    id: 17279,
+    first_name: "Third",
+    last_name: "Person",
+    list_id: 1158,
+    avatar_image_id: 266289,
+    urls: [],
+    images: [
+      {
+        id: 266289,
+        alg_type: 2,
+        image: "/api/v1/faces/17279/images/266289/image?imghash=62e2688d3c3c512b9e6d565669b1cc06",
+        quality: 0.7675786,
+        major_version: 20200706,
+        minor_version: 1,
+        image_hash: "62e2688d3c3c512b9e6d565669b1cc06",
+        face_id: 17279,
+        image_thumbnail: "/api/v1/faces/17279/images/266289/image-thumbnail?imghash=62e2688d3c3c512b9e6d565669b1cc06",
+        status: "OK"
+      }
+    ],
+    plates: [],
+    plates_count: 0
+  },
+  {
+    id: 17278,
+    first_name: "Second",
+    last_name: "Person",
+    list_id: 1158,
+    avatar_image_id: 266288,
+    urls: [],
+    images: [
+      {
+        id: 266288,
+        alg_type: 2,
+        image: "/api/v1/faces/17278/images/266288/image?imghash=9c0f369ccd4fdacf96a73b8bdb84daee",
+        quality: 0.5955136,
+        major_version: 20200706,
+        minor_version: 1,
+        image_hash: "9c0f369ccd4fdacf96a73b8bdb84daee",
+        face_id: 17278,
+        image_thumbnail: "/api/v1/faces/17278/images/266288/image-thumbnail?imghash=9c0f369ccd4fdacf96a73b8bdb84daee",
+        status: "OK"
+      }
+    ],
+    plates: [],
+    plates_count: 0
+  },
+  {
+    id: 17277,
+    first_name: "First",
+    last_name: "Person",
+    list_id: 1158,
+    avatar_image_id: 266287,
+    urls: [],
+    images: [
+      {
+        id: 266287,
+        alg_type: 2,
+        image: "/api/v1/faces/17277/images/266287/image?imghash=fa2dc1f00dbfbcd4ed711b85ae8296e5",
+        quality: 0.6383728,
+        major_version: 20200706,
+        minor_version: 1,
+        image_hash: "fa2dc1f00dbfbcd4ed711b85ae8296e5",
+        face_id: 17277,
+        image_thumbnail: "/api/v1/faces/17277/images/266287/image-thumbnail?imghash=fa2dc1f00dbfbcd4ed711b85ae8296e5",
+        status: "OK"
+      }
+    ],
+    plates: [],
+    plates_count: 0
   }
 ]
