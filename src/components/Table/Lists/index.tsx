cb8758bb8d69e6aeebf806c4a584263170e7c4cb
u1810291 <o.rizakulov@student.inha.uz>
2021-05-03 09:41:27 +0500

    console cleared

--- src/components/Table/Lists/index.tsx
+++ src/components/Table/Lists/index.tsx
@@ -2,7 +2,6 @@ import { Container, ImageWrap, Item, Image } from './style'
 
 const Images = ({ data }: any) => (
   <Container>
-    {console.log(data.map((el: any) => el))}
     {data.map((el: any, index: number) => {
       <Item key={index + 1}>
         <span>{el}</span>
