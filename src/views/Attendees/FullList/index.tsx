a59be878dac82925f43b61cdf5e398e48f7c6e2e
u1810291 <o.rizakulov@student.inha.uz>
2021-05-05 17:06:54 +0500

    some changes

--- src/views/Attendees/FullList/index.tsx
+++ src/views/Attendees/FullList/index.tsx
@@ -1,13 +1,13 @@
 import React, { useMemo } from 'react'
 import Table from '../../../components/Table'
-import { data, headers, links } from './helper'
+import { data, headers, links, footer } from './helper'
 
 export default () => {
   const column = useMemo(() => headers, []);
   return (
     <div>
       <h1>Full list</h1>
-      <Table headers={column} data={data} links={links} />
+      <Table headers={column} data={data} links={links} footer={footer} />
     </div>
   )
 }
