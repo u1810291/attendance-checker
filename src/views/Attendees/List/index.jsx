bf42419b02a2e8cf98fbf1aca56523b22ee091da
u1810291 <o.rizakulov@student.inha.uz>
2021-05-08 23:59:52 +0500

    modal

--- src/views/Attendees/List/index.jsx
+++ src/views/Attendees/List/index.jsx
@@ -1,7 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react'
-import Table from '../../../components/Table'
+import React, {useEffect} from 'react';
+import Table from '../../../components/Table';
+import {useDispatch} from 'react-redux'
+import {fetchData} from '../../../redux/modules/attendees/actions'
+
 export default ()=> {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(fetchData())
+  },[])
   return (
     <div>
       List
