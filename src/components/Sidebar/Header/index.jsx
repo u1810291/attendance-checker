392ea421a201e3fdf497a7e3aa21a3ef6df8879a
u1810291 <o.rizakulov@student.inha.uz>
2021-05-08 14:13:51 +0500

    some chjnages

--- src/components/Sidebar/Header/index.jsx
+++ src/components/Sidebar/Header/index.jsx
@@ -4,43 +4,22 @@ import { useHistory } from 'react-router-dom';
 import {
   Header,
   TitleContainer,
-  CollapseButton,
-  ExpandButton
 } from './style';
-import { useWindowSize } from '../../../hooks/use-window-size';
-import * as devices from '../../../constants/devices';
-import { ReactComponent as Logo } from '../../../assets/icons/logo3.svg';
+import Logo from '../../../assets/icons/logo512.png';
 
-const SidebarHeader = ({ collapsed, handleExpand, handleCollapse }) => {
+const SidebarHeader = () => {
   const history = useHistory();
-  const { device } = useWindowSize();
-  const mobile = (
+  const tabletDesktop = (
     <Header>
       <TitleContainer>
         <Logo className="pointer" onClick={() => { history.push('/profile'); }} />
-        <ExpandButton onClick={handleCollapse} />
       </TitleContainer>
     </Header>
   );
-  const tabletDesktop = (
-    <Header collapsed={collapsed}>
-      <TitleContainer collapsed={collapsed}>
-        { !collapsed && <Logo className="pointer" onClick={() => { history.push('/profile'); }} />}
-        { collapsed
-          ? <ExpandButton onClick={handleExpand} />
-          : <CollapseButton onClick={handleCollapse} />}
-      </TitleContainer>
-    </Header>
-  );
-  if (device === devices.MOBILE) return mobile;
-  if (device === devices.TABLET) return tabletDesktop;
-  if (device === devices.NOTEBOOK) return tabletDesktop;
-  if (device === devices.DESKTOP) return tabletDesktop;
-  return <></>;
+  return tabletDesktop
 };
 
 SidebarHeader.propTypes = {
-  collapsed: PropTypes.bool.isRequired,
   handleExpand: PropTypes.func.isRequired,
   handleCollapse: PropTypes.func.isRequired
 };
