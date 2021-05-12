5ad7f4054cec4c9bbc43cc6f789681169a98d070
u1810291 <o.rizakulov@student.inha.uz>
2021-05-12 14:58:09 +0500

    some changes

---
+++ src/components/Headers/PeopleList/index.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+import { Container } from './style'
+export default () => {
+  return (
+    <Container>
+      List Header
+    </Container>
+  )
+}
